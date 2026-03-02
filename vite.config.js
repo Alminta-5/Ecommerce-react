e854ab43856caa40e137a01c8a1948764208f842
Alminta <almintavj512004@gmail.com>
2026-03-02 12:49:33 +0530

    Feat: Completed checkout, responsiveness, and order management

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command, mode }) => {
   // during development we want the app to run at the root so that
@@ -7,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
   // Pages subpath. The production build on GH Pages lives under
   // `/Ecommerce-react/`, so we use that as the base there.
   return {
-    plugins: [react()],
+    plugins: [react(),tailwindcss()],
     base: mode === 'production' ? '/Ecommerce-react/' : '/',
   };
 });
