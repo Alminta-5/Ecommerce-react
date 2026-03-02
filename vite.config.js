7f99a61ebc422df96424339e0613970dcada2626
Alminta <almintavj512004@gmail.com>
2026-03-02 13:08:45 +0530

    Feat: Completed checkout, responsiveness, and order management

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command, mode }) => {
-  // during development we want the app to run at the root so that
-  // navigating to `/kidswear` etc. works without requiring the GitHub
-  // Pages subpath. The production build on GH Pages lives under
-  // `/Ecommerce-react/`, so we use that as the base there.
   return {
-    plugins: [react(),tailwindcss()],
-    base: mode === 'production' ? '/Ecommerce-react/' : '/',
+    plugins: [react(), tailwindcss()],
+    // If we are on Vercel, use '/', otherwise use the GH Pages path
+    base: process.env.VERCEL ? '/' : (mode === 'production' ? '/Ecommerce-react/' : '/'),
   };
 });
-
